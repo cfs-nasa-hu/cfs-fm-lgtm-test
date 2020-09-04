bec7f05fb615413fee0c0531793823fce66f30f4
cfs-nasa-hu <70075179+cfs-nasa-hu@users.noreply.github.com>
2020-09-04 17:09:12 -0500

    Update test_severity_error_credentials_lgtm.py

--- cfs_fm_lgtm_test/test_lgtm/test_severity_error_credentials_lgtm.py
+++ cfs_fm_lgtm_test/test_lgtm/test_severity_error_credentials_lgtm.py
@@ -1,5 +1,18 @@
 import hashlib
 import binascii
+import ssl
+import socket
+
+# Using the deprecated ssl.wrap_socket method
+ssl.wrap_socket(socket.socket(), ssl_version=ssl.PROTOCOL_SSLv2)
+
+# Using SSLContext
+context = ssl.SSLContext(ssl_version=ssl.PROTOCOL_SSLv3)
+
+# Using pyOpenSSL
+from pyOpenSSL import SSL
+context = SSL.Context(SSL.TLSv1_METHOD)
+
 
 CONFIG_FILE = 'passwords.json'
 
