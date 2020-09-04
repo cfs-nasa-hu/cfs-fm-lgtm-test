21601d9d0a7f4eca043bdc76cda04d562983561b
cfs-nasa-hu <70075179+cfs-nasa-hu@users.noreply.github.com>
2020-09-04 10:15:17 -0500

    Update test_severity_error_credentials_lgtm.py

--- cfs_fm_lgtm_test/test_lgtm/test_severity_error_credentials_lgtm.py
+++ cfs_fm_lgtm_test/test_lgtm/test_severity_error_credentials_lgtm.py
@@ -5,6 +5,9 @@ import binascii
 def redirect(args):
     pass
 
+def load_from_config(args, **kwargs):
+    pass
+
 def test_credentials_severity_error_lgtm(request):
     password = request.GET["password"]
     # BAD: 
@@ -20,5 +23,5 @@ def test_credentials_severity_error_lgtm(request):
     # Authentication made by comparing to a hash password from a config file.
     masked = hashlib.pbkdf2_hmac('sha256', password, load_config, 100000)
     hashed_input = binascii.hexlify(masked)
-    if hashed_input == hashed_password:
+    if hashed_input == hashed_pass:
         redirect("login")
